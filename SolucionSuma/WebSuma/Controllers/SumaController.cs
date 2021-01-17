9d3904ea3a7d7ad1fcbd1251f09ad32bc767ba68
Leonardo Noya garcia <a2019115506@estudiantes.upsa.edu.bo>
2021-01-17 16:01:34 -0400

    Suma en Get y Post

---
+++ SolucionSuma/WebSuma/Controllers/SumaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSuma.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SumaController : Controller
+    {
+        [HttpGet]
+      public int suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        [HttpPost]
+        public int sumar([FromHeader]int num1, [FromHeader]int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
